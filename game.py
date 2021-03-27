91d456df8f0d17c770c19769c892539d7aa0890c
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-03-27 19:45:42 -0400

    Basic info addeed

---
+++ game.py
@@ -0,0 +1,7 @@
+"""
+Boardgame with minigames! (needs a better title)
+Ben Life & Oliver Song
+HooHacks 2021
+Built on PyGame and Gamebox
+Gamebox was built by Luther Tychonievich (tychonievich@virginia.edu)
+"""
