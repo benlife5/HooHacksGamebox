e8548769debeb258b51610c42454b416bfe9b505
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-03-27 19:51:07 -0400

    Set up
    set up

--- game.py
+++ game.py
@@ -5,3 +5,15 @@ HooHacks 2021
 Built on PyGame and Gamebox
 Gamebox was built by Luther Tychonievich (tychonievich@virginia.edu)
 """
+
+import pygame
+import gamebox
+camera = gamebox.Camera(800,600)
+
+
+def tick(keys):
+    camera.clear('white')
+    camera.display()
+
+ticks = 30
+gamebox.timer_loop(ticks, tick)
