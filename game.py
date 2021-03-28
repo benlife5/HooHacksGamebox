c97e9ef4537707df02b21366b2896df724fcd7f9
Oliver Song <80020297+obs4qqf@users.noreply.github.com>
2021-03-27 20:36:37 -0400

    Initial if structure

--- game.py
+++ game.py
@@ -8,12 +8,20 @@ Gamebox was built by Luther Tychonievich (tychonievich@virginia.edu)
 
 import pygame
 import gamebox
-camera = gamebox.Camera(800,600)
+camera = gamebox.Camera(1000,700)
+miniGames = ["Matching Pairs"]
+miniGame = ""
 
+MPObjects = []
 
 def tick(keys):
     camera.clear('white')
+
+    if miniGame == "":
+
+
     camera.display()
+
 
 ticks = 30
 gamebox.timer_loop(ticks, tick)
